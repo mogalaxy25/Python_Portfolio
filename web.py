70f333363e854102a87c51ef395cec985306307f
mogalaxy25 <44922047+mogalaxy25@users.noreply.github.com>
2024-10-24 14:51:52 -0400

    initial Horuku deployment

--- web.py
+++ web.py
@@ -1,25 +1,26 @@
-import streamlit as sl
-import functions as fn
+import streamlit as st
+import functions
 
-todos = fn.get_todos()
+todos = functions.get_todos()
 
 def add_todo():
-    todo = sl.session_state["new_todo"] + "\n"
+    todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
-    fn.write_todos(todos)
+    functions.write_todos(todos)
 
-sl.title("My Todo App")
-sl.subheader("This is my todo app")
-sl.write("This app is to increase your productivity")
+
+st.title("My Todo App")
+st.subheader("This is my todo app.")
+st.write("This app is to increase your productivity.")
 
 
 for index, todo in enumerate(todos):
-    checkbox = sl.checkbox(todo, key=todo)
+    checkbox = st.checkbox(todo, key=todo)
     if checkbox:
         todos.pop(index)
-        fn.write_todos(todos)
-        del sl.session_state[todo]
-        sl.experimental_rerun(
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
-sl.text_input(label="Enter a todo", placeholder="Add new todo ...",
+st.text_input(label="", placeholder="Add new todo...",
               on_change=add_todo, key='new_todo')
